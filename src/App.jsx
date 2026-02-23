b33f696adb8d6f6faaeefa72cd38f899393c0565
mitalimanaktala <mitalimanaktala8@gmail.com>
2026-02-23 13:28:39 +0530

    feat: add automatic scroll to top on route change

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ import Profile from "./pages/Profile.jsx";
 import Search from "./pages/Search.jsx";
 import Trending from "./pages/Trending.jsx";
 import History from "./pages/History.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 import "./index.css";
 
 
 function App() {
   return (
     <Layout>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/trending" element={<Trending />} />
