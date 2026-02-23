0ba1d69332c4d129a9f1fe486cd82908cc621a70
mitalimanaktala <mitalimanaktala8@gmail.com>
2026-02-23 11:57:51 +0530

    refactor: update routing configuration and app entry setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
 import './App.css'
+import Layout from "./components/Layout.jsx";
+import Watch from "./pages/Watch.jsx";
+import Home from "./pages/Home.jsx";
+import Upload from "./pages/Upload.jsx";
+import Profile from "./pages/Profile.jsx";
+import Search from "./pages/Search.jsx";
+import Trending from "./pages/Trending.jsx";
+import History from "./pages/History.jsx";
+import "./index.css";
+
 
 function App() {
   return (
-    
-  )
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/trending" element={<Trending />} />
+        <Route path="/watch/:id" element={<Watch />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </Layout>
+  );
 }
 
-export default App
+export default App;
